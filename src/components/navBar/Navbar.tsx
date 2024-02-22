115bda621270fed63fcfb91e8b433b94297d4fae
lNalator <enzomidonetpro@gmail.com>
2024-02-22 23:21:25 +0100

    EM: added route navigation from navbar

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -2,27 +2,48 @@ import { Center, Image } from "@mantine/core";
 import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 import classes from "./Navbar.module.css";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+
+function NavPath(pathName: string) {
+  if (pathName === "/about") {
+    return {
+      image: "Contact.svg",
+      prevLink: "/projects",
+      nextLink: "/",
+    };
+  } else if (pathName === "/projects") {
+    return {
+      image: "Projets.svg",
+      prevLink: "/",
+      nextLink: "/about",
+    };
+  } else {
+    return {
+      image: "Acceuil.svg",
+      prevLink: "/about",
+      nextLink: "/projects",
+    };
+  }
+}
 
 export default function Navbar() {
+  const route = useRouter();
   const pathName = usePathname();
-
-  let imageUrl: string;
   console.log(pathName);
-  if (pathName === "/about") {
-    imageUrl = "Contact.svg";
-  } else if (pathName === "/projects") {
-    imageUrl = "Projets.svg";
-  } else {
-    imageUrl = "Acceuil.svg";
-  }
 
   return (
     <nav className={classes.navbar}>
-      <IconChevronUp className={classes.link} />
+      <IconChevronUp
+        className={classes.link}
+        onClick={() => route.push(NavPath(pathName).prevLink)}
+      />
       <Center>
-        <Image src={imageUrl as unknown as string} />
+        <Image src={NavPath(pathName).image} />
       </Center>
-      <IconChevronDown className={classes.link} />
+      <IconChevronDown
+        className={classes.link}
+        onClick={() => route.push(NavPath(pathName).nextLink)}
+      />
     </nav>
   );
 }
