bbab976919ba581006a2a82d337e32ff3f9d574d
lNalator <enzomidonetpro@gmail.com>
2024-03-01 15:19:56 +0100

    EM : changed card data to fit in projects Cards

--- src/components/data/cardsData.tsx
+++ src/components/data/cardsData.tsx
@@ -4,7 +4,7 @@ export const cardData = [
     title: "Arcade Runnin'",
     image: "./Images/Arcade Runnin.png",
     description:
-      "Arcade Runnin, était à la base un challenge que je m'était lancé après le passage de mon bac:\nCréer un jeu sous Unity pendant 1 semaine après 2 ans sans avoir programmé en C#. Je vous présente ici ce mini projet",
+      "Un  projet challenge que je m'était lancé après mon bac:\nCréer un jeu Unity en 1 semaine après 2 ans sans avoir programmé en C#.",
     date: "Janvier, 2020",
     projectType: "Personel",
   },
@@ -13,7 +13,7 @@ export const cardData = [
     title: "Corquette et Macadam",
     image: "./Images/croquettemacadam.png",
     description:
-      "Projet que j'ai réalisé en parallèle de mon alternance consistant à créer le site d'une association. Après analyse du besoin j'ai réalisé le site web via Wordpress puisque le client voulais pouvoir modifier et actualiser le site après que j'aie rendu le site.",
+      "Projet que j'ai réalisé en parallèle de mon alternance consistant à créer le site d'une association.",
     date: "AVRIL 2021 - SEP 2023",
     projectType: "Projet Bénévole",
   },
