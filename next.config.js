6e060dce9bf5c851436c7775fef63e62e048da99
lNalator <enzomidonetpro@gmail.com>
2024-03-01 11:47:00 +0100

    Hot fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: "export",
-// };
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+};
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
