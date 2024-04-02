d4b1b34419a81eb61bb118452f025e70b40f1591
lNalator <enzomidonetpro@gmail.com>
2024-04-02 19:47:51 +0200

    EM : added new project (Cultivation Craft)

--- src/components/data/cardsData.tsx
+++ src/components/data/cardsData.tsx
@@ -1,12 +1,12 @@
 export const cardData = [
   {
-    projectLink: "./Arcade Runnin.zip",
-    title: "Arcade Runnin'",
-    image: "./Images/Arcade Runnin.png",
+    projectLink: "https://www.curseforge.com/minecraft/mc-mods/cultivationcraft",
+    title: "CultivationCraft",
+    image: "./Images/CultivationCraft.png",
     description:
-      "Un  projet challenge que je m'était lancé après mon bac:\nCréer un jeu Unity en 1 semaine après 2 ans sans avoir programmé en C#.",
-    date: "Janvier, 2020",
-    projectType: "Personel",
+      "Projet de mod minecraft, se basant sur les novels chinois de 'Cultivation', je travail dessus avec un partenaire Australien.",
+    date: "Mars 2024 - Au présent",
+    projectType: "Personel - En Equipe",
   },
   {
     projectLink: "https://croquetteetmacadam.com/",
@@ -14,7 +14,18 @@ export const cardData = [
     image: "./Images/croquettemacadam.png",
     description:
       "Projet que j'ai réalisé en parallèle de mon alternance consistant à créer le site d'une association.",
-    date: "AVRIL 2021 - SEP 2023",
-    projectType: "Projet Bénévole",
+    date: "Avril 2021 - Septembre 2023",
+    projectType: "Bénévole - Client",
   },
+  {
+    projectLink: "./Arcade Runnin.zip",
+    title: "Arcade Runnin'",
+    image: "./Images/Arcade Runnin.png",
+    description:
+      "Un  projet challenge que je m'était lancé après mon bac:\nCréer un jeu Unity en 1 semaine après 2 ans sans avoir programmé en C#.",
+    date: "Janvier 2020",
+    projectType: "Personel - Solo",
+  },
+  
+
 ];
